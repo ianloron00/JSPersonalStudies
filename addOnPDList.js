ccbfb924c8927a5103475bdd655634c548a76135
Mr. Ian <ianloron00@gmail.com>
2021-02-03 00:33:22 -0300

    Drop-Down list dynamically constructed

--- addOnPDList.js
+++ addOnPDList.js
@@ -1,3 +1,6 @@
+// help:
+// https://www.techiedelight.com/dynamically-create-drop-down-list-javascript/
+
 var arrayPeople = new Array()
 
 class setPerson {
@@ -7,43 +10,27 @@ class setPerson {
     }
 }
 
-function addPerson(name, age) {
+document.getElementById("button").onclick = function addPerson() {
     var i = arrayPeople.length
-    if (name == "") name = "name_" + i
+    var name = document.getElementById("namePerson").value
+    var age = document.getElementById("agePerson").value
+    if (name == "" || name == undefined) name = "name_" + i
+    if (age == "" || age == undefined) age = "age_" + i
     arrayPeople[i] = new setPerson(name, age)
-    document.getElementById("selection").setAttribute(name, i)
-    document.getElementById("personSelected").innerHTML = "Person "+name+" and "+age+" was added.\n"
-}
-function showData(name, age) {
-    var n = document.getElementById("selection").nodeName
-    var a = document.getElementById("selection").nodeValue.age
-    document.getElementById("personSelected").innerHTML = n+" is "+a+" years old.\n"
-    document.getElementById("personSelected").innerHTML = name+" iss "+age+" yearss old.\n"
-}
-
-
-/*
-function showData(form) {
-    var len = form.arrayPeople.length
-    for (var p = 0; p < len; p++) {
-        var res = arrayPeople[p].name + " has " + 
-                  arrayPeople[p].age + " years old.\n"
-    }
-    form.output.value = res
+    
+    var select = document.getElementById("selection")
+    var option = document.createElement("option")
+    option.text = name 
+    option.id = i
+    select.appendChild(option)
+    document.getElementById("personSelected").innerHTML = "Person "+name+" ("+age+" years) was added.\n"
 }
 
-function selectData() {
-    var page = ""
-    page += "<select name='people' onchange='showData(this.form)'> " 
-    for(var j = 0; j < arrayPeople.length; j++) {
-        page += "<option"
-        if (j==0) {
-            page += " selected"
-        }
-        page += ">" + arrayPeople[j].name
-    }
-
-    page += "</select><p>"
-    document.write(page)
+document.getElementById("selection").onchange = function showData() {
+    var elem = document.getElementById("selection") 
+    var id = elem.options[elem.selectedIndex].id
+    document.getElementById("personSelected").innerHTML = "id " + id + ".\n"
+    var name = arrayPeople[parseInt(id)].name 
+    var age = arrayPeople[parseInt(id)].age
+    document.getElementById("personSelected").innerHTML = name+" is "+age+" years old.\n"
 }
-*/
